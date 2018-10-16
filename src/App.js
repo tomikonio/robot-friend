63b61f598fd74721ca3166a6a8f46f71b8bb0a14
Igor <igrr69@gmail.com>
2018-10-16 12:18:53 +0300

    Add a cardlist component - 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './Card';
+import Cardlist from './Cardlist';
 import { robots } from './robots';
 
 class App extends Component {
   render() {
-    return (
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    );
+    return <Cardlist robots={robots} />;
   }
 }
 
---
+++ src/Cardlist.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+const Cardlist = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot, i) => {
+        return (
+          <Card
+            key={robot.id}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cardlist;
