83a50a4d6afba106fde4be93c6e155d4891fe063
Igor <igrr69@gmail.com>
2018-10-18 14:37:25 +0300

    Replace the robots.js with a call to an online api

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 // import './App.css';
 import Cardlist from './Cardlist';
 import Searchbox from './Searchbox';
-import { robots } from './robots';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}))
+      .catch(error => console.log('error getting users'));
   }
 
   onSearchChange = event => {
